749e3ab9c9539a1173f5cf3a73ee4a56523cc162
GOUACHE-Nathan-2225041 <123471299+GOUACHE-Nathan-2225041aa@users.noreply.github.com>
2024-10-02 12:09:24 +0200

    Update spook4.sh

--- spook4.sh
+++ spook4.sh
@@ -1,4 +1,4 @@
-amixer -D pulse set Master 50% unmute
+amixer -D pulse set Master 75% unmute
 (ffplay -nodisp -nostats -hide_banner -autoexit https://www.myinstants.com/media/sounds/fnaf-freddys-music-box.mp3 && ffplay -nodisp -nostats -hide_banner -autoexit https://www.myinstants.com/media/sounds/power-outage-sound-effect.mp3) &
 number=1.00
 while (( $(bc <<< "$number >= 0") )); do
